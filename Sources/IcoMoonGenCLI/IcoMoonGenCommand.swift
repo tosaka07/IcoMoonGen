08d40c7e1ceb590f0ecc2df4f63ad1db071b6385
tosaka07 <tosakaup@gmail.com>
2021-10-12 11:46:50 +0900

    Update to 0.0.2

--- Sources/IcoMoonGenCLI/IcoMoonGenCommand.swift
+++ Sources/IcoMoonGenCLI/IcoMoonGenCommand.swift
@@ -4,7 +4,7 @@ public struct IcoMoonGenCommand: ParsableCommand {
     public static var configuration: CommandConfiguration = CommandConfiguration(
         commandName: "icomoongen",
         abstract: "A icon font code generator.",
-        version: "0.0.1",
+        version: "0.0.2",
         shouldDisplay: true,
         subcommands: [GenerateCommand.self],
         defaultSubcommand: GenerateCommand.self)
